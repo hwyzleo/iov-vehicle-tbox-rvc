e2942007fce28b7304d1c89ea975ff835bb5d3a8
hwyz_leo <hwyz.leo.ye@gmail.com>
2024-09-11 23:30:39 +0800

    杂项: 寻车增加python调用，验证整体链路

--- src/find_vehicle.cpp
+++ src/find_vehicle.cpp
@@ -6,12 +6,13 @@
 #include "../third_party/include/spdlog/spdlog.h"
 
 #include "find_vehicle.h"
+#include <cstdlib>
 
 /**
  * 寻车
  */
 void FindVehicle::ControlCmd(const void *payload, int payload_len) {
-
+    system("python3 /home/jetson/hwyz/test.py");
 }
 
 void FindVehicle::OnStart() {
@@ -28,4 +29,5 @@ void FindVehicle::OnError() {
 
 void FindVehicle::Handle(void *payload, int payload_len) {
     spdlog::debug("处理寻车指令[{}]", std::string(static_cast<char *>(payload), payload_len));
+    ControlCmd(payload, payload_len);
 }
--- src/tbox_mqtt_config.cpp
+++ src/tbox_mqtt_config.cpp
@@ -9,6 +9,8 @@
 
 TboxMqttConfig::TboxMqttConfig() : mqtt_config_() {
     mqtt_config_.subscribe_topics = subscribe_topics_;
+    mqtt_config_.server_host = server_host_;
+    mqtt_config_.server_port = server_port_;
 }
 
 TboxMqttConfig::~TboxMqttConfig() = default;
@@ -18,13 +20,11 @@ TboxMqttConfig &TboxMqttConfig::GetInstance() {
     return instance;
 }
 
-bool TboxMqttConfig::SetInfo(const std::string &server_host, const std::string &username, const std::string &client_id) {
+bool TboxMqttConfig::SetInfo(const std::string &username, const std::string &client_id) {
     spdlog::info("设置TBOX MQTT配置信息");
-    if (server_host.empty() || username.empty() || client_id.empty()) {
+    if (username.empty() || client_id.empty()) {
         return false;
     }
-    mqtt_config_.server_host = server_host;
-    mqtt_config_.server_port = 1884;
     mqtt_config_.username = username;
     mqtt_config_.client_id = client_id;
     return GeneratePassword();
